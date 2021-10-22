dcb465c9c9cf44c87467fe621b28b1b2f284d74f
jasmme <92761024+jasmme@users.noreply.github.com>
2021-10-22 11:24:06 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var urName = ;
 
 function saveName() {
-  urName = document.getElementById("nameOfUser").value;
+  //get value from input field and save as "a"
+  urName = document.getElementById('nameOfUser').value;
+  
+  //create LS key with value of "a"
+  localStorage.setItem('uName',a);
+  
+  //automatically take user to page two.html
+  location.href = "two.html";
+  
   window.alert(urName);
 }
+
 
 
 function thisWontWork() {
