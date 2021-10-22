56e4e6fcb3c6089b23706c7f51022f0190dbc98c
jasmme <92761024+jasmme@users.noreply.github.com>
2021-10-22 11:47:04 -1000

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var a;
 
 function saveName() {
   //get value from input field and save as "a"
-  a = document.getElementById('nameOfUser').value;
+  a = document.getElementById("nameOfUser").value;
   document.getElementById("nameOfUser").value = "";
   
   //create LS key with value of "a"
-  localStorage.setItem('uName',a);
+  localStorage.setItem("uName",a);
   
   //automatically take user to page two.html
   location.href = "two.html";
@@ -15,6 +15,6 @@ function saveName() {
 
 
 function getUser() {
-  var b = localStorage.getItem('uName');
+  var b = localStorage.getItem("uName");
   document.getElementById("placeholder").innerHTML = "hello " + b; 
 }
